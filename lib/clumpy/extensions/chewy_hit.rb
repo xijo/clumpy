cc82906dafd8f55c1ce03cb1ff0216d077cc48b1
Johannes Opper <xijo@gmx.de>
2014-05-27 21:08:52 +0200

    Fixes lat/lng/id access

--- lib/clumpy/extensions/chewy_hit.rb
+++ lib/clumpy/extensions/chewy_hit.rb
@@ -2,16 +2,16 @@ module Clumpy
   module Extensions
     module ChewyHit
       def latitude
-        @latitude ||= lat.to_f
+        @latitude ||= _source.lat.to_f
       end
 
       def longitude
-        @longitude ||= lng.to_f
+        @longitude ||= _source.lng.to_f
       end
 
       def as_json(*)
         {
-          id:   id,
+          id:   _source.id,
           type: _source.full?(:type) || type.classify
         }
       end
