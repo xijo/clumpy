a2e5259f098e77698394113cec5ded9a7cc01478
Johannes Opper <johannes.opper@gmail.com>
2014-10-30 16:24:04 +0100

    Remove hit extensions, mechanism is deprecated

--- lib/clumpy.rb
+++ lib/clumpy.rb
@@ -3,8 +3,6 @@ require "clumpy/version"
 require "clumpy/cluster_behavior"
 require "clumpy/cluster"
 require "clumpy/builder"
-require "clumpy/extensions/sunspot_hit"
-require "clumpy/extensions/chewy_hit"
 require "clumpy/bounds"
 
 module Clumpy
